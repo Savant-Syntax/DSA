001e07e59b1e404d30ce65ca289c8dba3860c669
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-13 20:40:58 +0530

    Square pattern question

---
+++ classes/class4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Pattern Question
+
+    // 1. Square Pattern (for numbers 1 to n)
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 1.2 Square Pattern in star form
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+}
