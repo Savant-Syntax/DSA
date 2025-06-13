0a29c51b8e41d56a1cbcd17a4a903c1595cfee89
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-13 21:21:21 +0530

    Square pattern questions

--- classes/class4.cpp
+++ classes/class4.cpp
@@ -18,7 +18,7 @@ int main(){
     //     cout << endl;
     // }
 
-    // 1.2 Square Pattern in star form
+    // 1.2 Square Pattern in Star form
     // int n;
     // cout << "Enter the value of n: ";
     // cin >> n;
@@ -30,4 +30,38 @@ int main(){
     //     }
     //     cout << endl;
     // }
+
+    // 1.3 Square Pattern in Alphabetic form
+    // int n;
+    // char ch ='A';
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A';
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << ch;
+    //         ch = ch + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // 1.4 Square Pattern in continue form (from number 1 to n)
+    // int n;
+    // int num = 1;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+    // cout << "After pattern completion the value of num: "<< num;    // Checking the process
+
+    return 0;
 }
