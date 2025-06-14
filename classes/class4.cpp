1004c12025ca0a6e46f1c33b39b2146ca4aa5a99
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-14 22:18:12 +0530

    Triangle Pattern Problems

--- classes/class4.cpp
+++ classes/class4.cpp
@@ -64,19 +64,90 @@ int main(){
     // cout << "After pattern completion the value of num: "<< num;    // Checking the process
 
     // Homework
-    // Square Pattern in continue form in Alphabetic form (from character A to n)
-    int n;
-    char ch = 'A';
-    cout << "Enter the value of n: ";
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << ch << " ";
-            ch++;
-        }
-        cout << endl;
-    }
-    return 0;
+    // Square Pattern in continue form in Alphabetic form (from character A to)
+    // int n;
+    // char ch = 'A';
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Triangle pattern
+
+    // 1.1 Right Angled Triangle (using star)
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    
+    // 1.2 Right Angled Triangle (using number in continous increasing manner)
+    // int n;
+    // int num = 1;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout << num++ << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 1.3 Right Angled Triangle (using number, each row has same number and the number is in continous manner for the columns.)
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout << i+1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 1.4 Right Angled Triangle 
+    // int n;
+    // int num = 1;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 1; j < i+1; j++)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 1.5 Reverse Right Angle Triangle
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i+1; j > 0; j--)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+    
+
+    // return 0;
 }
