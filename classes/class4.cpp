ba820f35405890afd1532215f9154b2fe6cd365b
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-16 22:22:02 +0530

    Hollow Diamondd Pattern

--- classes/class4.cpp
+++ classes/class4.cpp
@@ -191,6 +191,54 @@ int main(){
     //     }
     //     cout << endl;
     // }
+
+    //Hollow Diamond Pattern
+    // int n;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    // // top
+    // for (int i = 0; i < n; i++)
+    // {
+    //     // spaces
+    //     for (int j = 0; j < n-i-1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+    //     if (i != 0)
+    //     {
+    //             // spaces
+    //             for (int j = 0; j < 2*i-1; j++)
+    //         {
+    //             cout << " ";
+    //         }
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // // bottom
+    // for (int i = 0; i < n-1; i++)
+    // {
+    //     // spaces
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+    //     if (i != n-2)
+    //     {
+    //         // spaces
+    //         for (int j = 0; j < 2*(n-i)-5; j++)
+    //         {
+    //             cout << " ";
+    //         }
+            
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    
     
     return 0;
 }
