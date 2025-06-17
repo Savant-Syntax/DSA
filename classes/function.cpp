d6c7aa45fb09b7b8d9ddec57725b93f120ef1c70
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-17 20:40:50 +0530

    function

---
+++ classes/function.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int printHello(){                    // function definition
+    cout << "Hello World \n";
+    return 5;
+}
+
+int sum(int a, int b){              // int a, int b => Parameters => copies of arguments
+    int s = a + b;
+}
+
+int min(int a, int b){
+    if (a > b)
+    {
+        return a;
+    }   else    {
+        return b;
+    }
+}
+
+// Calculate sum of number from 1 to n
+int sumN(int n){
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int main(){
+    cout << printHello() << endl;   // function call
+    cout << sum(5, 8) << endl;      // 5, 8 => Arguments => actual values
+    cout << min(8, 4) << endl;
+    cout << sumN(6) << endl;
+}
