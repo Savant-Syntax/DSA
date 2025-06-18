5b6650183d1e8536f83f45c91471228e0a9bc940
Savant-Syntax <vinitastrak04@gmail.com>
2025-06-18 21:17:08 +0530

    Calculate n factorial

--- classes/function.cpp
+++ classes/function.cpp
@@ -29,9 +29,20 @@ int sumN(int n){
     return sum;
 }
 
+// Calulate  N factorial
+int factorialN(int n){
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
 int main(){
     cout << printHello() << endl;   // function call
     cout << sum(5, 8) << endl;      // 5, 8 => Arguments => actual values
     cout << min(8, 4) << endl;
     cout << sumN(6) << endl;
+    cout << factorialN(6) << endl;
 }
